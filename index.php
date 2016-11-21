022de76ab20281414f9f072c86bb236cb6111b81
eggertaxels <eggertgeir@gmail.com>
2016-11-21 14:43:03 +0000

    head bætt við

--- index.php
+++ index.php
@@ -1,7 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Hópverkefni</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="hopverkefni.css">
+    <link rel="stylesheet" href="normalize.css">
 </head>
 <body>
     <h1>Eggerts Skrá</h1>
