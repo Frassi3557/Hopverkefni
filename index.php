84e89e428395887d5616d8a312d7345b5115e9fa
eggertaxels <eggertgeir@gmail.com>
2016-11-21 14:51:18 +0000

    script í css

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="hopverkefni.css">
     <link rel="stylesheet" href="normalize.css">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
 <body>
     <h1>Eggerts Skrá</h1>
