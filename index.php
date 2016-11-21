bd1f3dafdc798d16f7b8c710d6675845a0853fd1
eggertaxels <eggertgeir@gmail.com>
2016-11-21 14:53:24 +0000

    sett í möppu

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 	<title>Hópverkefni</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="hopverkefni.css">
-    <link rel="stylesheet" href="normalize.css">
+    <link rel="stylesheet" href="css/hopverkefni.css">
+    <link rel="stylesheet" href="css/normalize.css">
     <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 </head>
