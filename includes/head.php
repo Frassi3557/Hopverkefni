3865f631c46996aae39169072a0422af56c1cb0c
frassi3557 <freysi_freys@live.com>
2016-11-23 11:22:23 +0000

    set gögn í head.php, leturgerð, stylesheet o.s.frv.

--- includes/head.php
+++ includes/head.php
@@ -1,1 +1,6 @@
-
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" type="text/css" href="stylesheets/normalize.css">
+<link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css">
+<link rel="icon" href="http://www.tskoli.is/skin/basic9k/i/fav-large.png">
+<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
