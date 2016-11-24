8b2a9daf5667678b4d99885886c831edb34f526e
eggertaxels <eggertgeir@gmail.com>
2016-11-24 12:03:21 +0000

    form sett inn í box

--- baeta.php
+++ baeta.php
@@ -10,14 +10,20 @@
 </head>
 <body>
     <form action="includes/json_baeta.php" method="post">
-        <fieldset>
-            <p>JSON</p>
-            Orðtak <input type="text" name="ordtak" required><br>
-            Útskýring <input type="text" name="utskyra" required><br>
-            Uppruni <input type="text" name="uppruni" required><br>
-            Dæmi <input type="text" name="daemi" required><br>
-            <input type="submit">
-        </fieldset>  
+        <div class="group">
+			<h1><input type="text" name="ordtak" placeholder="Orðtak" required></h1>
+			<div class="ordtak">
+				<div class="utskyring">
+					<input type="text" name="utskyra" placeholder="Útskýring" required>
+				</div>
+				<input type="text" name="uppruni" placeholder="Uppruni" required>
+				<div class="daemi">
+					<p class="daemi-text">Dæmi:</p>
+					<input type="text" name="daemi" placeholder="Dæmi" required>
+				</div>
+			</div>
+		</div>
+            <input type="submit">  
     </form>
 </body>
 </html>
