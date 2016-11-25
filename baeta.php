a9676e6e34870acf8c776114a0e9f37b4bb76db5
frassi3557 <freysi_freys@live.com>
2016-11-25 10:06:01 +0000

    bæti við gögnum í index.php

--- baeta.php
+++ baeta.php
@@ -1,24 +1,26 @@
 <!DOCTYPE html>
 <html>
 <head>
-	
+  <?php
+      require($_SERVER['DOCUMENT_ROOT'] . "/2T/1811992029/hopverkefni/includes/head.php");
+    ?>
 </head>
 <body>
-    <form action="includes/json_baeta.php" method="post">
-        <div class="group">
-			<h1><input type="text" name="ordtak" placeholder="Orðtak" required></h1>
-			<div class="ordtak">
-				<div class="utskyring">
-					<input type="text" name="utskyra" placeholder="Útskýring" required>
-				</div>
-                <textarea rows="5" cols="40" name="uppruni" placeholder="Uppruni" required></textarea>
-				<div class="daemi">
-					<p class="daemi-text">Dæmi:</p>
-					<input type="text" name="daemi" placeholder="Dæmi" required>
-				</div>
-			</div>
-		</div>
-            <input class="sub" type="submit">  
-    </form>
+<form action="includes/json_baeta.php" method="post">
+  <div class="group">
+    <h1><input type="text" name="ordtak" placeholder="Orðtak" required></h1>
+    <div class="ordtak">
+      <div class="utskyring">
+        <input type="text" name="utskyra" placeholder="Útskýring" required>
+      </div>
+      <textarea rows="5" cols="40" name="uppruni" placeholder="Uppruni" required></textarea>
+      <div class="daemi">
+        <p class="daemi-text">Dæmi:</p>
+        <input type="text" name="daemi" placeholder="Dæmi" required>
+      </div>
+    </div>
+  </div>
+  <input class="sub" type="submit">  
+</form>
 </body>
 </html>
--- index.php
+++ index.php
@@ -1,1 +1,31 @@
-
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>title</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!-- Foreach lykkja fer í gegnum multidimensional fylkið sem heldur utan um orðtökin -->
+    <?php foreach($ordtok as $key => $value): ?>
+    <!-- Klasi sem heldur utan um allar upplýsingarnar í fylkinu -->
+    <div class="group">
+      <!-- Skrifar út orðtakið í h1 -->
+      <h1><?php echo $ordtok[$key]['ordtak']?></h1>
+      <!-- Klasi sem heldur utan um upplýsingar um orðtakið -->
+      <div class="ordtak">
+        <!-- Klasi sem heldur utan um útskýringuna á orðtakinu -->
+        <div class="utskyring">
+          <p><?php echo $ordtok[$key]['utskyring']?></p>
+        </div>
+        <p><?php echo $ordtok[$key]['uppruni']?></p>
+        <!-- Klasi sem heldur utan um dæmið -->
+        <div class="daemi">
+          <p class="daemi-text">Dæmi:</p>
+          <p><?php echo $ordtok[$key]['daemi']?></p>
+        </div>
+      </div>
+    </div>
+    <!-- Lokar lykkjunni -->
+    <?php endforeach; ?>
+  </body>
+</html>
