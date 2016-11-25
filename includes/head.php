6ca83529aa7ade1811c1e69427456692b4cf5f79
frassi3557 <freysi_freys@live.com>
2016-11-25 11:12:44 +0000

    laga eitt orðtak, breyti leturgerð

--- includes/head.php
+++ includes/head.php
@@ -3,4 +3,4 @@
 <link rel="stylesheet" type="text/css" href="stylesheets/normalize.css">
 <link rel="stylesheet" type="text/css" href="stylesheets/stylesheet.css">
 <link rel="icon" href="http://www.tskoli.is/skin/basic9k/i/fav-large.png">
-<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Cormorant+Garamond:600" rel="stylesheet">
