087faafd67c05482074693ee230a8c83fa2700f0
frassi3557 <freysi_freys@live.com>
2016-11-25 11:42:26 +0000

    bý til header / navigation bar

--- includes/header.php
+++ includes/header.php
@@ -1,1 +1,6 @@
-
+<?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
+<div class="header-nav">
+	<nav>
+		<li><a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>>Orðtök</a></li><li><a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>>Bæta við orðtaki</a></li>
+	</nav>
+</div>
