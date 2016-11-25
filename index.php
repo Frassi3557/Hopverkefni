6bd961c319caf26f40ac3e7249ab6a253f79d582
frassi3557 <freysi_freys@live.com>
2016-11-25 10:51:46 +0000

    læt index síðu birta öll orðtökin úr json skránni

--- index.php
+++ index.php
@@ -5,27 +5,26 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    <!-- Foreach lykkja fer í gegnum multidimensional fylkið sem heldur utan um orðtökin -->
-    <?php foreach($ordtok as $key => $value): ?>
-    <!-- Klasi sem heldur utan um allar upplýsingarnar í fylkinu -->
+    <?php
+      $string_json = file_get_contents('ordtok/ordtok.json');
+        json_ordtok = json_decode($string_json, true);
+        for ($x = 0; $x <= count($json_ordtok['ordtokFylki'])-1; $x++):
+    ?>
     <div class="group">
-      <!-- Skrifar út orðtakið í h1 -->
-      <h1><?php echo $ordtok[$key]['ordtak']?></h1>
+      <h1><?php echo ucfirst($json_ordtok['ordtokFylki'][$x]['ordtak']) ?></h1>
       <!-- Klasi sem heldur utan um upplýsingar um orðtakið -->
       <div class="ordtak">
         <!-- Klasi sem heldur utan um útskýringuna á orðtakinu -->
         <div class="utskyring">
-          <p><?php echo $ordtok[$key]['utskyring']?></p>
+          <p><?php ucfirst($json_ordtok['ordtokFylki'][$x]['utskyring']) ?></p>
         </div>
-        <p><?php echo $ordtok[$key]['uppruni']?></p>
+        <p><?php ucfirst($json_ordtok['ordtokFylki'][$x]['uppruni']) ?></p>
         <!-- Klasi sem heldur utan um dæmið -->
         <div class="daemi">
           <p class="daemi-text">Dæmi:</p>
-          <p><?php echo $ordtok[$key]['daemi']?></p>
+          <p><?php ucfirst($json_ordtok['ordtokFylki'][$x]['daemi']) ?></p>
         </div>
       </div>
     </div>
-    <!-- Lokar lykkjunni -->
-    <?php endforeach; ?>
   </body>
 </html>
