b17b2dd7c363d49bb9a5d5ab36e452057097c6f1
eggertaxels <eggertgeir@gmail.com>
2016-11-28 13:49:14 +0000

    bæta mynd síða búin til

--- includes/header.php
+++ includes/header.php
@@ -3,5 +3,6 @@
 	<nav>
 		<li><a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>>Orðtök</a></li>
         <li><a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>>Bæta við orðtaki</a></li>
+        <li><a href="baetamynd.php"<?php if ($currentPage == 'baetamynd.php') {echo 'id="here"';}?>>Bæta við mynd</a></li>
 	</nav>
 </div>
