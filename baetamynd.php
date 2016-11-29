1ac6cefd86a6ea819f13f224c4c5520f2904edee
eggertaxels <eggertgeir@gmail.com>
2016-11-29 08:57:59 +0000

    form bætt við fyrir myndir

--- baetamynd.php
+++ baetamynd.php
@@ -10,6 +10,21 @@
 </head>
 <body>
   <?php require($_SERVER['DOCUMENT_ROOT'] . "/2T/0506992429/hopverkefni/includes/header.php"); ?>
-    
+    <div class="myndform">
+        <h1>Endilega bættu við mynd í myndasafnið.</h1>
+        <form action="json_baeta_mynd.php" method="post">
+            <fieldset>
+                <label for="mname">Nafn á mynd</label>
+                <input type="text" id="mname" name="myndNafn" required>
+
+                <label for="url">URL á mynd</label>
+                <input type="text" id="url" name="myndaslod" required>
+
+                <br><br>
+
+                <input type="submit" value="BIRTA">
+            </fieldset>
+        </form>
+    </div>
 </body>
 </html>
