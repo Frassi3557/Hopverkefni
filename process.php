3a86d65083cc564d84c969d0966b71a1367876bf
frassi3557 <freysi_freys@live.com>
2016-12-01 11:45:32 +0000

    bæti við smá texta í process.php og læt submit takka vísa í upload.php

--- process.php
+++ process.php
@@ -9,11 +9,11 @@
   <body>
     <?php require($_SERVER['DOCUMENT_ROOT'] . "/2T/1811992029/hopverkefni/includes/header.php");
       $url = $_POST['url'] ?>
-      <form action="myndir.php" method="post">
+      <form action="upload.php" method="post">
         <div class="upload">
           <input class="nafn" type="text" name="nafn" required placeholder="Nafn / Titill">
           <img class="mynd-preview" src="<?= $url ?>">
-          <textarea class="description"></textarea>
+          <textarea class="description" placeholder="Lýsing / nánari upplýsingar um myndina"></textarea>
           <br><br>
           <input class="sub" type="submit" value="birta mynd">
         </div>
