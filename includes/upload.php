12b98c01f9a77d947615279404827f2e318ce666
frassi3557 <freysi_freys@live.com>
2016-12-01 12:05:49 +0000

    læt upload.php setja mynd sem notandi velur að hlaða inn á vefinn inn í fylki í myndir.json

---
+++ includes/upload.php
@@ -0,0 +1,26 @@
+<?php
+	$url = $_POST['url'];
+	$nafn = $_POST['nafn'];
+	$description = $_POST['description'];
+	//get from file
+    $jso = file_get_contents('../ordtok/myndir.json');
+    //dceode object from file
+    $obj = json_decode($jso);
+    //display error
+    if (!$obj) trigger_error('JSON FAIL: ' .  json_last_error(), E_USER_ERROR);
+
+    // CREATE SOME NEW DATA TO APPEND TO THE komp ARRAY
+    $new = new StdClass;
+    $new->nafn = $nafn;
+    $new->url = $url;
+    $new->description = $description;
+
+    // INJECT SOME DATA INTO THE OBJECT
+    $obj->myndirFylki[] = $new;
+
+
+    // RENDER THE NEW JSON STRING
+    //encode back to json for storage
+    $final_obj = json_encode($obj);
+    file_put_contents('../ordtok/myndir.json', $final_obj);
+    header('Location:http://tsuts.tskoli.is/2t/0506992429/hopverkefni/myndir.php');
--- process.php
+++ process.php
@@ -9,11 +9,12 @@
   <body>
     <?php require($_SERVER['DOCUMENT_ROOT'] . "/2T/1811992029/hopverkefni/includes/header.php");
       $url = $_POST['url'] ?>
-      <form action="upload.php" method="post">
+      <form action="includes/upload.php" method="post">
         <div class="upload">
+					<input class="mynd-url" name="url" value="<?= $url ?>">
           <input class="nafn" type="text" name="nafn" required placeholder="Nafn / Titill">
           <img class="mynd-preview" src="<?= $url ?>">
-          <textarea class="description" placeholder="Lýsing / nánari upplýsingar um myndina"></textarea>
+          <textarea class="description" name="description" placeholder="Lýsing / nánari upplýsingar um myndina"></textarea>
           <br><br>
           <input class="sub" type="submit" value="birta mynd">
         </div>
