073ce4a6fa1561f3881bfbb536023780a1ca6f6d
eggertaxels <eggertgeir@gmail.com>
2016-12-01 10:57:15 +0000

    laga villur

--- includes/title.php
+++ includes/title.php
@@ -6,10 +6,7 @@ if ($title == 'index') {
 }
 if ($title == 'baeta') {
  $title = 'Bæta við orðtaki';
-<<<<<<< HEAD
-=======
 }
 if ($title == 'baetamynd') {
  $title = 'Bæta við mynd';
->>>>>>> 8eef5ca09cb84f7a2cd078a9277dc983d0e83ff7
 }
