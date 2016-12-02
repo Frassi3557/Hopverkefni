2dc1c41ce4d324560210800f8082a8547552a56d
eggertaxels <eggertgeir@gmail.com>
2016-12-02 11:09:56 +0000

    reyna að gera drop down menu

--- includes/header.php
+++ includes/header.php
@@ -1,9 +1,13 @@
 <?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
-<div class="header-nav">
-	<nav>
-		<li><a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>>Orðtök</a></li>
-        <li><a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>>Bæta við orðtaki</a></li>
-        <li><a href="myndir.php"<?php if ($currentPage == 'myndir.php') {echo 'id="here"';}?>>Myndir</a></li>
-        <li><a href="baetamynd.php"<?php if ($currentPage == 'baetamynd.php') {echo 'id="here"';}?>>Bæta við mynd</a></li>
-	</nav>
+<div>
+    <input class="input1" type="checkbox" id="toggle">
+    <label class="checkbox" for="toggle">&#9776; Menu</label>
+    <nav class="menu">
+        <ul class="topmenu">
+            <li><a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>>Orðtök</a></li>
+            <li><a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>>Bæta við orðtaki</a></li>
+            <li><a href="myndir.php"<?php if ($currentPage == 'myndir.php') {echo 'id="here"';}?>>Myndir</a></li>
+            <li><a href="baetamynd.php"<?php if ($currentPage == 'baetamynd.php') {echo 'id="here"';}?>>Bæta við mynd</a></li>
+        </ul>
+    </nav>
 </div>
