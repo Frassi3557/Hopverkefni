973aa9cd5a2ce0d2f777295eb83a936946bea68d
Eggert Axelsson <Eggert Axelsson>
2016-12-02 14:34:05 +0000

    drop down menu komið, á eftir að breyta lit

--- includes/header.php
+++ includes/header.php
@@ -1,13 +1,15 @@
-<?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
-<div>
-    <input class="input1" type="checkbox" id="toggle">
-    <label class="checkbox" for="toggle">&#9776; Menu</label>
-    <nav class="menu">
-        <ul class="topmenu">
-            <a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>><li>Orðtök</li></a>
-            <a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>><li>Bæta við orðtaki</li></a>
-            <a href="myndir.php"<?php if ($currentPage == 'myndir.php') {echo 'id="here"';}?>><li>Myndir</li></a>
-            <a href="baetamynd.php"<?php if ($currentPage == 'baetamynd.php') {echo 'id="here"';}?>><li>Bæta við mynd</li></a>
-        </ul>
-    </nav>
-</div>
+<header>
+    <?php $currentPage = basename($_SERVER['SCRIPT_FILENAME']); ?>
+    <div>
+        <input class="input1" type="checkbox" id="toggle">
+        <label class="checkbox" for="toggle">&#9776; Menu</label>
+        <nav class="menu">
+            <ul class="topmenu">
+                <a href="index.php"<?php if ($currentPage == 'index.php') {echo 'id="here"';}?>><li>Orðtök</li></a>
+                <a href="baeta.php"<?php if ($currentPage == 'baeta.php') {echo 'id="here"';}?>><li>Bæta við orðtaki</li></a>
+                <a href="myndir.php"<?php if ($currentPage == 'myndir.php') {echo 'id="here"';}?>><li>Myndir</li></a>
+                <a href="baetamynd.php"<?php if ($currentPage == 'baetamynd.php') {echo 'id="here"';}?>><li>Bæta við mynd</li></a>
+            </ul>
+        </nav>
+    </div>
+</header>
